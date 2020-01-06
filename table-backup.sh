65999633ecac2c3aa37f583476557ce08e07f94e
urgodfather <5252428+urgodfather@users.noreply.github.com>
2020-01-06 17:06:21 -0500

    Update table-backup.sh

--- table-backup.sh
+++ table-backup.sh
@@ -10,21 +10,22 @@
 #    specified on command-line.
 #  * Script will truncate table after.
 
-[ $# -lt 3 ] && echo "Usage: $(basename $0) <DB_HOST> <DB_USER> <DB_NAME> [<DIR>] <t>" && exit 1
+[ $# -lt 3 ] && echo "Usage: $(basename $0) <DB_NAME> [<DIR>] <t>" && exit 1
 
-DB_host=$1
+#DB_host=$1
 #DB_user=$2
-DB=$2
-DIR=$3
-t=$4
+DB=$1
+DIR=$2
+t=$3
 
 [ -n "$DIR" ] || DIR=.
 test -d $DIR || mkdir -p $DIR
 
 tbl_count=0
 
-mysqldump -h $DB_host $DB $t | gzip > $DIR/$DB.$t.`date +"\%Y-\%m-\%d"`.sql.gz
-mysql -NBA -h $DB_host -D $DB -e 'truncate table '"$t;"'
+mysqldump  $DB $t | gzip > $DIR/$DB.$t.`date +"\%Y-\%m-\%d"`.sql.gz & BACK_PID=$!
+wait $BACK_PID
+mysql -NBA -D $DB -e 'truncate table '"$t;"''
 
 tbl_count=$(( tbl_count + 1 ))
 
