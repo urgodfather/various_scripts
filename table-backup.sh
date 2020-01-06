e8694391c8467f8374bdfa55999c9f789fa0cf4d
urgodfather <5252428+urgodfather@users.noreply.github.com>
2020-01-06 16:17:42 -0500

    Update table-backup.sh

--- table-backup.sh
+++ table-backup.sh
@@ -13,17 +13,17 @@
 [ $# -lt 3 ] && echo "Usage: $(basename $0) <DB_HOST> <DB_USER> <DB_NAME> [<DIR>] <t>" && exit 1
 
 DB_host=$1
-DB_user=$2
-DB=$3
-DIR=$4
-t=$5
+#DB_user=$2
+DB=$2
+DIR=$3
+t=$4
 
 [ -n "$DIR" ] || DIR=.
 test -d $DIR || mkdir -p $DIR
 
 tbl_count=0
 
-mysqldump -h $DB_host $DB.$t | gzip > $DIR/$DB.$t.`date +"\%Y-\%m-\%d"`.sql.gz
+mysqldump -h $DB_host $DB $t | gzip > $DIR/$DB.$t.`date +"\%Y-\%m-\%d"`.sql.gz
 mysql -NBA -h $DB_host -D $DB -e 'truncate table '"$t;"'
 
 tbl_count=$(( tbl_count + 1 ))
